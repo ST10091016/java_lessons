4564981a412916c3b58207c919863c738045aa5e
TumiMaleka <tumimaleka2002@gmail.com>
2024-02-28 08:38:26 +0200

    Challenge 1 complete

--- fundamentals/19_challenge_1/DateValidator.java
+++ fundamentals/19_challenge_1/DateValidator.java
@@ -1,27 +1,40 @@
 import java.util.Date;
 import java.time.*;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 
 public class DateValidator{
+    
+    //method that checks if the date format is valid
+    public static boolean isValid(String dob){
 
-	public static bool isValid(String dob){
+      //creating an object for the date format class
+      SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+      
+      //initialising the variable to null as its not specified yet
+      Date dateOfBirth = null;
 
-    DateFormat format = DateFormat.ofPattern("dd/MM/yyyy");
-    Date dateOfBirth = null;
-    format.setLenient(false);
+      //specfies if the date format should be strict or not, in this case since it is false it will be strict and will not allow invalid values
+      dateFormat.setLenient(false);
 
-    //allows the code to be tested for any errors on the date while its being executed.
-    try{
+      //if statement that checks if the dob is empty
+      if(dob.isEmpty()){
+
+        System.out.println("\nYour date of birth cannot be empty. \nPlease re-enter your birth date in the format.\n");
+        return false;
+      }
+  
+      //allows the dob to be tested during execution, if an error occurs then it goes to the catch
+      try{
+
+         dateOfBirth = dateFormat.parse(dob);
+         return true;
+
+      } catch(Exception e){
+         
+        System.out.println("\nInvalid date input. \nPlease use the correct (DD/MM/YYYY) format.\n");
+        return false;
+      } 
  
-    	dateOfBirth = format.parse(dob);
-    	return true;
-
-    }catch(Exception e){
-
-       return false;
-
-    }
-
-	}
+    }   
+      
 }
--- fundamentals/19_challenge_1/Prompter.java
+++ fundamentals/19_challenge_1/Prompter.java
@@ -1,39 +1,18 @@
 import java.util.Scanner;
 
 public class Prompter{
+ 
+  //method that prompts the user for an input
+  public static String prompt(String prompt){
+    
+   System.out.print(prompt);
+    
+   //creating an object for the scanner class and assigning it to a variable
+   Scanner input = new Scanner(System.in);
 
-	public static string prompt(){
+   //returning the String value 
+   return input.nextLine();
 
-		//instantiation of Scanner class
-        Scanner sc = new Scanner(System.in);
-
-        //prompting the user
-		System.out.print("What is your name: ");
-		String name = sc.nextLine();
-
-		System.out.print("What is your surname: ");
-		String surname = sc.nextLine();
-
-		System.out.print("Enter your date of birth (DD/MM/YYYY): ");
-        String date_of_birth = sc.nextLine();
-
-        //instantiation of DateValidator class
-        DateValidator dv = new DateValidator();
-
-        if(dv.isValid(date_of_birth)){
-            
-            System.out.print("Valid date of birth");
-
-        }else{
-
-        	System.out.print("Invalid date of birth");
-        }
-
-        //stored as a string since the user might add km
-        System.out.print("How far is your favourite store from your home? ");
-        String distance_from_store = sc.nextLine();
-
-        return name + " " + surname + " " + date_of_birth + " " + distance;
-	}
+  }
 		
 }
